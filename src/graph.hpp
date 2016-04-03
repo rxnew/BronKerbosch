06d9c3b2d46462a85e2fc2f14da5107a765dbd36
rxnew <rxnew.axdseuan@gmail.com>
2016-04-03 16:13:26 +0000

    Graphクラス完成

--- src/graph.hpp
+++ src/graph.hpp
@@ -10,16 +10,16 @@ template <class V>
 class Graph {
  public:
   using Vertices = std::unordered_set<V*>;
-  using AdjacencyList = std::unordered_map<V*, Vertices>;
+  using Edges = std::unordered_map<V*, Vertices>;
 
  private:
   Vertices vertices_;
-  AdjacencyList adjacency_;
+  Edges edges_;
 
  public:
   Graph() = default;
   template <class T>
-  explicit Graph(T&& adjacency);
+  explicit Graph(T&& vertices);
   ~Graph() = default;
   auto addVertice(V* v) -> void;
   template <class... Rest>
@@ -31,8 +31,7 @@ class Graph {
 
 template <class V>
 template <class T>
-inline Graph<V>::Graph(T&& adjacency)
-  : adjacency_(std::forward<T>(adjacency)) {
+inline Graph<V>::Graph(T&& vertices) : vertices_(std::forward<T>(vertices)) {
 }
 
 template <class V>
@@ -44,22 +43,21 @@ template <class V>
 template <class... Rest>
 auto Graph<V>::addEdge(V* v1, V* v2, Rest... rest)
   -> void {
-  this->adjacency_[v1].insert(v2);
-  this->adjacency_[v2].insert(v1);
+  this->edges_[v1].insert(v2);
+  this->edges_[v2].insert(v1);
   if(sizeof...(Rest)) addEdge(v1, rest...);
 }
 
 template <class V>
 template <class... Rest>
-auto Graph<V>::removeEdge(V* v1, V* v2, Rest... rest)
-  -> void {
-  this->adjacency_[v1].erase(v2);
-  this->adjacency_[v2].erase(v1);
+auto Graph<V>::removeEdge(V* v1, V* v2, Rest... rest) -> void {
+  this->edges_[v1].erase(v2);
+  this->edges_[v2].erase(v1);
   if(sizeof...(Rest)) removeEdge(v1, rest...);
 }
 
 template <class V>
 inline auto Graph<V>::existEdge(V* v1, V* v2) -> bool {
-  return this->adjacency_[v1].count(v2);
+  return this->edges_[v1].count(v2);
 }
 }
